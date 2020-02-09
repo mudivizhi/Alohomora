c0f69ef4d4204a3289d3af0f553cd71389e5d02b
srinivasan-devaraj <srinivasan16.devaraj@gmail.com>
2020-02-10 01:34:48 +0530

    Revert "Revert "Revert "OTP and Google SignIn Functionalities Added"""
    This reverts commit 34fa93af

--- app/src/main/java/com/mudivizhi/alohomora/MainActivity.java
+++ app/src/main/java/com/mudivizhi/alohomora/MainActivity.java
@@ -1,27 +1,18 @@
 package com.mudivizhi.alohomora;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button categorySelect;
+    Button profileSelect;
 
-    private GoogleSignInClient googleSignInClient;
 
 
     @Override
@@ -31,10 +22,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tb);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        googleSignInClient = GoogleSignIn.getClient(this,gso);
         // Get the ActionBar here to configure the way it behaves.
         final ActionBar ab = getSupportActionBar();
         //ab.setHomeAsUpIndicator(R.drawable.ic_menu); // set a custom icon for the default home button
@@ -44,36 +31,22 @@ public class MainActivity extends AppCompatActivity {
         ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
 
         categorySelect = findViewById(R.id.selectCategoryButton);
-        categorySelect.setOnClickListener((view)->{moveToNextActivity();});
+        categorySelect.setOnClickListener((view)->{moveToCategoryActivity();});
+
+        profileSelect = findViewById(R.id.selectProfileButton);
+        profileSelect.setOnClickListener((view)->{moveToProfileActivity();});
+
 
     }
-    private void moveToNextActivity() {
+    private void moveToCategoryActivity() {
 
         Intent intent = new Intent(this,Category.class);
         startActivity(intent);
-
     }
+    private void moveToProfileActivity() {
 
-    public void logOutCurrentUser(View view){
-        Intent thisIntent = getIntent();
-        String signInMethod = thisIntent.getStringExtra("signInMethod");
-        if(signInMethod.equals("phone")) {
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }else if(signInMethod.equals("google")){
-            googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    //On Succesfull signout we navigate the user back to LoginActivity
-                    Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                }
-            });
+        Intent intent = new Intent(this,AccountDetails.class);
+        startActivity(intent);
 
-        }
-        MainActivity.this.finish();
     }
 }
