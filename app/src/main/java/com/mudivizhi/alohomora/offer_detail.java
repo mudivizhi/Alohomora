729a45a019ebd9a1b7edf828feb2640768e355c2
santhosh2514 <santhosh.25na@gmail.com>
2020-02-12 22:23:09 +0530

    single offer detail design done

---
+++ app/src/main/java/com/mudivizhi/alohomora/offer_detail.java
@@ -0,0 +1,15 @@
+package com.mudivizhi.alohomora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class offer_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_detail);
+    }
+
+}
