a9a8d4814ba6a49005fb17714e632bd2be6aa7eb
santhosh2514 <santhosh.25na@gmail.com>
2020-02-13 01:17:37 +0530

    code copy java done

--- app/src/main/java/com/mudivizhi/alohomora/Category.java
+++ app/src/main/java/com/mudivizhi/alohomora/Category.java
@@ -2,53 +2,58 @@ package com.mudivizhi.alohomora;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.widget.Button;
 
 public class Category extends Fragment {
-
+    CardView uberselect;
 
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.activity_category,container,false);
     }
-    private static final String TAG = "Category";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//    private static final String TAG = "Category";
+//    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        uberselect = getActivity().findViewById(R.id.uber);
 //        setContentView(R.layout.activity_category);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
+        uberselect.setOnClickListener((view) -> {
+            moveToSingleCategoryActivity();
         });
-
-
     }
-
+    private void moveToSingleCategoryActivity() {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(),offer_detail.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/mudivizhi/alohomora/offer_detail.java
+++ app/src/main/java/com/mudivizhi/alohomora/offer_detail.java
@@ -2,14 +2,33 @@ package com.mudivizhi.alohomora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class offer_detail extends AppCompatActivity {
 
+    TextView coupon_text;
+
+    TextView copy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_detail);
-    }
 
+        copy = (TextView) findViewById(R.id.coupon_code);
+        copy.setOnClickListener((view) -> {
+            copycode();
+        });
+    }
+//
+    private void copycode(){
+            ClipboardManager manager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+            TextView coupon=(TextView)findViewById(R.id.coupon_code);
+            String coupon_text=coupon.getText().toString();
+            ClipData clipData=ClipData.newPlainText("text",coupon_text);
+            manager.setPrimaryClip(clipData);
+    }
 }
